c08d3bff31a89439af12b48d0e96fb048c640186
Gabriel Penteado <105011647+penteado-git@users.noreply.github.com>
2022-06-24 11:08:18 -0300

    Add files via upload

---
+++ usersList.ts
@@ -0,0 +1,42 @@
+class usersList {
+  private users = new Array<User>();
+
+  userRegister(user: User) {
+    if (this.users.length == 0) {
+      this.users.push(user);
+    } else {
+      for (let i in this.users) {
+        if (this.users[i].getCode() == user.getCode()) {
+          alert("Código já cadastrado");
+        } else {
+          this.users.push(user);
+          alert("Usuario Cadastrado!");
+        }
+      }
+    }
+  }
+
+  listUsers(): void {
+    if (this.users.length == 0) {
+      console.log("Lista vazia");
+    }
+    console.clear();
+    for (let i in this.users) {
+      console.log(this.users[i]);
+    }
+  }
+
+  checkUser(user: User): User {
+    for (let i in this.users) {
+      if (this.users[i].getCode() === user.getCode()) {
+        user.setName(this.users[i].getName());
+        console.log("Usuario encontrado");
+        return user;
+      }
+    }
+    console.log("Usuario não existe");
+    user.setName("");
+    user.setCode(0);
+    return user;
+  }
+}
