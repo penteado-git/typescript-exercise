7357e34e2cbd3db2a0d83fdc5384f94bb01e208a
Gabriel Penteado <105011647+penteado-git@users.noreply.github.com>
2022-06-27 09:00:27 -0300

    Update usersList.js

--- dist/class/usersList.js
+++ dist/class/usersList.js
@@ -1,43 +1,44 @@
+import { User } from "./user";
+
 export class usersList {
-    constructor() {
-        this.users = new Array();
+  private users = new Array<User>();
+
+  userRegister(user: User) {
+    if (this.users.length == 0) {
+      this.users.push(user);
+    } else {
+      for (let i in this.users) {
+        if (this.users[i].getCode() == user.getCode()) {
+          alert("Código já cadastrado");
+        } else {
+          this.users.push(user);
+          alert("Usuario Cadastrado!");
+        }
+      }
     }
-    userRegister(user) {
-        if (this.users.length == 0) {
-            this.users.push(user);
-        }
-        else {
-            for (let i in this.users) {
-                if (this.users[i].getCode() == user.getCode()) {
-                    alert("Código já cadastrado");
-                }
-                else {
-                    this.users.push(user);
-                    alert("Usuario Cadastrado!");
-                }
-            }
-        }
+  }
+
+  listUsers(): void {
+    if (this.users.length == 0) {
+      console.log("Lista vazia");
     }
-    listUsers() {
-        if (this.users.length == 0) {
-            console.log("Lista vazia");
-        }
-        console.clear();
-        for (let i in this.users) {
-            console.log(this.users[i]);
-        }
+    console.clear();
+    for (let i in this.users) {
+      console.log(this.users[i]);
     }
-    checkUser(user) {
-        for (let i in this.users) {
-            if (this.users[i].getCode() === user.getCode()) {
-                user.setName(this.users[i].getName());
-                console.log("Usuario encontrado");
-                return user;
-            }
-        }
-        console.log("Usuario não existe");
-        user.setName("");
-        user.setCode(0);
+  }
+
+  checkUser(user: User): User {
+    for (let i in this.users) {
+      if (this.users[i].getCode() === user.getCode()) {
+        user.setName(this.users[i].getName());
+        console.log("Usuario encontrado");
         return user;
+      }
     }
+    console.log("Usuario não existe");
+    user.setName("");
+    user.setCode(0);
+    return user;
+  }
 }
